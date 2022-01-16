3f5b38ed0926b84ebee77c5141c8e84bec023b0c
lllllan <342310798@qq.com>
2022-01-16 21:25:40 +0800

    借助p名称空间注入属性

--- 3.Spring5/SpringTest/src/P7/BookTest.java
+++ 3.Spring5/SpringTest/src/P7/BookTest.java
@@ -10,7 +10,9 @@ public class BookTest {
     public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         Book book = context.getBean("book", Book.class);
+        Book book2 = context.getBean("book2", Book.class);
 
         System.out.println(book.getBookName());
+        System.out.println(book2.getBookName());
     }
 }
