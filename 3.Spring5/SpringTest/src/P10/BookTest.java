bf54aaa0e87a7b88d93ba7c835994c0d2926438f
lllllan <342310798@qq.com>
2022-01-16 22:40:19 +0800

    注入特殊字符

--- 3.Spring5/SpringTest/src/P10/BookTest.java
+++ 3.Spring5/SpringTest/src/P10/BookTest.java
@@ -10,7 +10,11 @@ public class BookTest {
     public void test() {
         ApplicationContext context = new ClassPathXmlApplicationContext("spring-config.xml");
         Book bean = context.getBean("null-book", Book.class);
+        Book bean1 = context.getBean("sp-book", Book.class);
+        Book bean2 = context.getBean("sp-book2", Book.class);
 
         System.out.println(bean.getName());
+        System.out.println(bean1.getName());
+        System.out.println(bean2.getName());
     }
 }
